3fa285596025b4538c0535ca7f8de157ee207834
Aurelie Binet <au.binet21@gmail.com>
2023-02-01 12:00:01 +0100

    commit 42

---
+++ main.js/index.js
@@ -0,0 +1,12 @@
+<button name="button">Réserver</button>;
+<form action="" method="get" class="form-example">
+<div class="form-example">
+<label for="name">Enter your name: </label>
+<input type="text" name="name" id="name" required />
+</div>
+
+<form action="" method="get" class="form-example">
+<div class="form-example">
+<label for="nombre de couverts">Enter your name: </label>
+<input type="nombre de couverts" name="nombre de couverts" id="nombre de couverts" required />
+</div>
