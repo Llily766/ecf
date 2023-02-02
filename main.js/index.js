ce9738751d35fb301d5167b17b2bae9538ed1ceb
Aurelie Binet <au.binet21@gmail.com>
2023-02-02 08:31:10 +0100

    COMMIT 44

--- main.js/index.js
+++ main.js/index.js
@@ -1,2 +1,27 @@
 <button name="button">Réserver</button>;
 
+<form action="" method="get" class="form-example">
+<div class="form-example">
+    <label for="le nombre de convives">Enter your name: </label>
+    <input type="le nombre de convives" name="le nombre de convives" id="le nombre de convives" required />
+    </div>
+
+<div class="form-example">
+    <label for="date">Enter your nom: </label>
+    <input type="date" name="date" id="date" required />
+    </div>
+
+<div class="form-example">
+      <label for="heure prévue">Enter your mot de passe: </label>
+      <input type="heure prévue" name="heure prévue" id="heure prévue" required />
+      </div>
+ 
+<div class="form-example">
+      <label for="Allergies" name="Allergies" id="Allergies" required />
+      <input type="Allergies" name="Allergies" id="Allergies" required />
+      </div>   
+
+<input type="submit" value="RESERVER" >
+    </form>
+
+
