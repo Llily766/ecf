c837f08776b95f04e85d9bbbf44b360e3fa6cc8f
Aurelie Binet <au.binet21@gmail.com>
2023-02-02 06:02:55 +0100

    commit 43

--- main.js/index.js
+++ main.js/index.js
@@ -1,29 +1,30 @@
 <button name="button">Réserver</button>;
-form action="" method="get" class="form-example">;
+
+<form action="" method="get" class="form-example">;
 <div class="form-example">;
 <label for="name">Enter your name: </label>;
 <input type="text" name="name" id="name" required />;
 </div>
 
-form action="" method="get" class="form-example">;
+<form> action="" method="get" class="form-example">;
 <div class="form-example">;
 <label for="nombre de couverts">Enter your name: </label>;
 <input type="nombre de couverts" name="nombre de couverts" id="nombre de couverts" required />;
 </div>
 
-form action="" method="get" class="form-example">;
+<form action="" method="get" class="form-example">;
 <div class="form-example">;
 <label for="date">Enter your name: </label>;
 <input type="date" name="date" id="date" required />;
 </div>
 
-form action="" method="get" class="form-example">;
+<form action="" method="get" class="form-example">;
 <div class="form-example">;
 <label for="l'heure prévue">Enter your name: </label>;
 <input type="l'heure prévue" name="l'heure prévue" id="l'heure prévue" required />;
 </div>
 
-form action="" method="get" class="form-example">;
+<form action="" method="get" class="form-example">;
 <div class="form-example">;
 <label for="Allergies">Enter your name: </label>;
 <input type="Allergies" name="Allergies" id="Allergies" required />;
